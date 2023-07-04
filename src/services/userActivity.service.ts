4a0b4efd86c7d51282b1d11d7abd649a33b1214f
SuryaRaj <jay303frengy@gmail.com>
2023-07-04 10:41:19 +0530

    bug fixed in login activity and user activity

--- src/services/userActivity.service.ts
+++ src/services/userActivity.service.ts
@@ -6,13 +6,13 @@ import { Service } from 'typedi';
 export class UserActivityService {
   public async getUserActivity(userid, params: any, page) {
     const { filter, limit, skip, sort } = <any>aqp(params);
-    const activity = await UserActivityModel.find({ userId: userid, ...filter })
+    const activity = await UserActivityModel.find({ user: userid, ...filter })
       .populate(['user', 'course'])
       .sort(sort)
       .limit(limit)
       .skip(skip);
 
-    const total_count = await UserActivityModel.find({ userId: userid, ...filter }).count();
+    const total_count = await UserActivityModel.find({ user: userid, ...filter }).count();
 
     return {
       activity,
